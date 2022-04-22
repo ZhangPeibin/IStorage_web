dccd5e7d2267244437d10a9fec68331c8a6c5f08
Peibin Zhang <ZhangPeibin@users.noreply.github.com>
2022-04-22 13:31:10 +0800

    delete unsed  library

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,12 @@ import 'antd/dist/antd.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/style.scss'
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import "node_modules/video-react/dist/video-react.css";
-import {SnackbarProvider} from "notistack";
 
 function MyApp({Component, pageProps}) {
     return (
-        <SnackbarProvider>
+        <>
             <Component {...pageProps} />
-        </SnackbarProvider>
+        </>
     )
 }
 
